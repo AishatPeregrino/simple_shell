5e598dcf0162bc618f3d9b14873a1006f43a0ae9
nancy-konadu <nancykyiadom994@gmail.com>
2023-07-25 08:42:53 -0700

    errors

--- errors.c
+++ errors.c
@@ -2,7 +2,7 @@
 
 /**
  *_eputs - prints an input string
- * @str: the string to be printed AishatPeregrino nancykonadu
+ * @str: the string to be printed AishatPeregrino NancyKonadu
  *
  * Return: Nothing
  */
